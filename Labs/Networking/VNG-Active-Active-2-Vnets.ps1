ca1c92e7d3c03e449c7b5382d5e77169ed4acf83
Romulo Brandino da Silva <romulo.brandino@gmail.com>
2020-10-19 15:29:57 -0500

    AZ

--- Labs/Networking/VNG-Active-Active-2-Vnets.ps1
+++ Labs/Networking/VNG-Active-Active-2-Vnets.ps1
@@ -1,7 +1,7 @@
 
 # Step 1 - Create and configure VNet1
 
-# 1. Declare your variables, If you need
+# 1. Declare your variables, If you need it.
 
 Connect-AzAccount
 Select-AzSubscription -SubscriptionName $Sub1
@@ -65,6 +65,45 @@ $gw1pip1 = Get-AzPublicIpAddress -Name $GW1IPName1 -ResourceGroupName $RG1
 $gw1pip2 = Get-AzPublicIpAddress -Name $GW1IPName2 -ResourceGroupName $RG1
 $vnet1gw = Get-AzVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1
 
+# Get the PIP GW
+$gw1pip1.IpAddress
+# PS /home/romulo> $gw1pip1.IpAddress
+# 52.188.171.7
+
+$gw1pip2.IpAddress
+# PS /home/romulo> $gw1pip2.IpAddress
+# 52.188.171.15
+
+$vnet1gw.BgpSettingsText 
+PS /home/romulo> $vnet1gw.BgpSettingsText
+{
+  "Asn": 65010,
+  "BgpPeeringAddress": "10.12.255.4,10.12.255.5",
+  "PeerWeight": 0,
+  "BgpPeeringAddresses": [
+    {
+      "IpconfigurationId": "/subscriptions/xyz/resourceGroups/v-romubr/providers/Microsoft.Network/virtualNetworkGateways/VNet1GW/ipConfigurations/gw1ipconf1",
+      "DefaultBgpIpAddresses": [
+        "10.12.255.4"
+      ],
+      "CustomBgpIpAddresses": [],
+      "TunnelIpAddresses": [
+        "52.188.171.7"
+      ]
+    },
+    {
+      "IpconfigurationId": "/subscriptions/xyz/resourceGroups/v-romubr/providers/Microsoft.Network/virtualNetworkGateways/VNet1GW/ipConfigurations/gw1ipconf2",
+      "DefaultBgpIpAddresses": [
+        "10.12.255.5"
+      ],
+      "CustomBgpIpAddresses": [],
+      "TunnelIpAddresses": [
+        "52.188.171.15"
+      ]
+    }
+  ]
+}
+
 # Part 2 - Establish an active-active cross-premises connection
 
 # Step 1 - Create and configure the local network gateway
@@ -72,7 +111,7 @@ $vnet1gw = Get-AzVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1
 # 1. Declare your variables
 
 $RG5 = "v-romubr"
-$Location5 = "West US"
+$Location5 = "East US"
 $LNGName51 = "Site5_1"
 $LNGPrefix51 = "10.52.255.253/32"
 $LNGIP51 = "131.107.72.22"
@@ -108,3 +147,103 @@ New-AzVirtualNetworkGatewayConnection -Name $Connection152 -ResourceGroupName $R
 # 3. VPN and BGP parameters for your second on-premises VPN device
 
 # Part 3 - Establish an active-active VNet-to-VNet connection
+
+# Step 1 - Create TestVNet2 and the VPN gateway
+
+# 1. Declare your variables
+
+$RG2 = "v-romubr"
+$Location2 = "West US"
+$VNetName2 = "TestVNet2"
+$FESubName2 = "FrontEnd"
+$BESubName2 = "Backend"
+$GWSubName2 = "GatewaySubnet"
+$VNetPrefix21 = "10.21.0.0/16"
+$VNetPrefix22 = "10.22.0.0/16"
+$FESubPrefix2 = "10.21.0.0/24"
+$BESubPrefix2 = "10.22.0.0/24"
+$GWSubPrefix2 = "10.22.255.0/27"
+$VNet2ASN = 65020
+$DNS2 = "8.8.8.8"
+$GWName2 = "VNet2GW"
+$GW2IPName1 = "VNet2GWIP1"
+$GW2IPconf1 = "gw2ipconf1"
+$GW2IPName2 = "VNet2GWIP2"
+$GW2IPconf2 = "gw2ipconf2"
+$Connection21 = "VNet2toVNet1"
+$Connection12 = "VNet1toVNet2"
+
+# 2. Create TestVNet2 in the new resource group
+
+New-AzResourceGroup -Name $RG2 -Location $Location2
+
+$fesub2 = New-AzVirtualNetworkSubnetConfig -Name $FESubName2 -AddressPrefix $FESubPrefix2
+$besub2 = New-AzVirtualNetworkSubnetConfig -Name $BESubName2 -AddressPrefix $BESubPrefix2
+$gwsub2 = New-AzVirtualNetworkSubnetConfig -Name $GWSubName2 -AddressPrefix $GWSubPrefix2
+
+New-AzVirtualNetwork -Name $VNetName2 -ResourceGroupName $RG2 -Location $Location2 -AddressPrefix $VNetPrefix21,$VNetPrefix22 -Subnet $fesub2,$besub2,$gwsub2
+
+# 3. Create the active-active VPN gateway for TestVNet2
+$gw2pip1 = New-AzPublicIpAddress -Name $GW2IPName1 -ResourceGroupName $RG2 -Location $Location2 -AllocationMethod Dynamic
+$gw2pip2 = New-AzPublicIpAddress -Name $GW2IPName2 -ResourceGroupName $RG2 -Location $Location2 -AllocationMethod Dynamic
+
+$vnet2 = Get-AzVirtualNetwork -Name $VNetName2 -ResourceGroupName $RG2
+$subnet2 = Get-AzVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $vnet2
+$gw2ipconf1 = New-AzVirtualNetworkGatewayIpConfig -Name $GW2IPconf1 -Subnet $subnet2 -PublicIpAddress $gw2pip1
+$gw2ipconf2 = New-AzVirtualNetworkGatewayIpConfig -Name $GW2IPconf2 -Subnet $subnet2 -PublicIpAddress $gw2pip2
+
+New-AzVirtualNetworkGateway -Name $GWName2 -ResourceGroupName $RG2 -Location $Location2 -IpConfigurations $gw2ipconf1,$gw2ipconf2 -GatewayType Vpn -VpnType RouteBased -GatewaySku VpnGw1 -Asn $VNet2ASN -EnableActiveActiveFeature
+
+# Step 2 - Connect the TestVNet1 and TestVNet2 gateways
+
+# 1. Get both gateways
+$vnet1gw = Get-AzVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1
+$vnet2gw = Get-AzVirtualNetworkGateway -Name $GWName2 -ResourceGroupName $RG2
+
+# 2. Create both connections
+
+New-AzVirtualNetworkGatewayConnection -Name $Connection12 -ResourceGroupName $RG1 -VirtualNetworkGateway1 $vnet1gw -VirtualNetworkGateway2 $vnet2gw -Location $Location1 -ConnectionType Vnet2Vnet -SharedKey 'AzureA1b2C3' -EnableBgp $True
+
+New-AzVirtualNetworkGatewayConnection -Name $Connection21 -ResourceGroupName $RG2 -VirtualNetworkGateway1 $vnet2gw -VirtualNetworkGateway2 $vnet1gw -Location $Location2 -ConnectionType Vnet2Vnet -SharedKey 'AzureA1b2C3' -EnableBgp $True
+
+# Change an active-standby gateway to an active-active gateway
+
+# 1. Declare your variables
+
+$GWName = "TestVNetAA1GW"
+$VNetName = "TestVNetAA1"
+$RG = "TestVPNActiveActive01"
+$GWIPName2 = "gwpip2"
+$GWIPconf2 = "gw1ipconf2"
+
+# After declaring the variables, you can copy and paste this example to your PowerShell console.
+
+$vnet = Get-AzVirtualNetwork -Name $VNetName -ResourceGroupName $RG
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -VirtualNetwork $vnet
+$gw = Get-AzVirtualNetworkGateway -Name $GWName -ResourceGroupName $RG
+$location = $gw.Location
+
+# 2. Create the public IP address, then add the second gateway IP configuration
+$gwpip2 = New-AzPublicIpAddress -Name $GWIPName2 -ResourceGroupName $RG -Location $location -AllocationMethod Dynamic
+Add-AzVirtualNetworkGatewayIpConfig -VirtualNetworkGateway $gw -Name $GWIPconf2 -Subnet $subnet -PublicIpAddress $gwpip2
+
+# 3. Enable active-active mode and update the gateway
+Set-AzVirtualNetworkGateway -VirtualNetworkGateway $gw -EnableActiveActiveFeature -GatewaySku HighPerformance
+
+# Change an active-active gateway to an active-standby gateway
+
+# 1. Declare your variables
+
+$GWName = "TestVNetAA1GW"
+$RG = "TestVPNActiveActive01"
+
+$gw = Get-AzVirtualNetworkGateway -Name $GWName -ResourceGroupName $RG
+$ipconfname = $gw.IpConfigurations[1].Name
+
+2. Remove the gateway IP configuration and disable the active-active mode
+Remove-AzVirtualNetworkGatewayIpConfig -Name $ipconfname -VirtualNetworkGateway $gw
+Set-AzVirtualNetworkGateway -VirtualNetworkGateway $gw -DisableActiveActiveFeature
+
+
+# https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-activeactive-rm-powershell
+
